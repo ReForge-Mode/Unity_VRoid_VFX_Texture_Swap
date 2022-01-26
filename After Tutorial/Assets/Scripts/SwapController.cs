1343f83a57c6187379e6b3ed5e12e19b3ff5820c
FFaUniHan <handersondavistjia@yahoo.com>
2022-01-27 05:39:59 +0700

    Initial Commit
    Initial Commit

--- After Tutorial/Assets/Scripts/SwapController.cs
+++ After Tutorial/Assets/Scripts/SwapController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SwapController : MonoBehaviour
 {
-    public Animator anim;      //Reference to Vita's animator component
+    public Animator anim;               //Reference to Vita's animator component
+    public SwapTexture swapTexture;
 
     private void OnGUI()
     {
@@ -12,12 +13,13 @@ public class SwapController : MonoBehaviour
         //This is a great tool for a quick debug if your script doesn't work.
         //But for a more permanent UI button, I recommend using the UI gameObject instead.
 
-        if (GUI.Button(new Rect(10,10, 200, 50), "Switch Pose!"))
+        if (GUI.Button(new Rect(10, 10, 200, 50), "Switch Pose!"))
         {
             //Triggers the Animator's parameter to change the animation.
             //You can open up the Animator window to see how it is set up.
 
             anim.SetTrigger("SwitchPose");
+            swapTexture.Invoke("swapTexture", 0.3f);
         }
     }
 }
